59032259413e1e0a467e66f6f84a96c60c46c659
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2024-11-27 15:52:57 -0300

    main code

--- main/main.c
+++ main/main.c
@@ -6,58 +6,52 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+
 #include "bmi270.c"
-#include "driver/uart.h"
-
-// Function for sending things to UART1
-static int uart1_printf(const char *str, va_list ap) {
-  char *buf;
-  vasprintf(&buf, str, ap);
-  uart_write_bytes(UART_NUM_1, buf, strlen(buf));
-  free(buf);
-  return 0;
-}
-
-// Setup of UART connections 0 and 1, and try to redirect logs to UART1 if asked
-static void uart_setup() {
-  uart_config_t uart_config = {
-      .baud_rate = 115200,
-      .data_bits = UART_DATA_8_BITS,
-      .parity = UART_PARITY_DISABLE,
-      .stop_bits = UART_STOP_BITS_1,
-      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-  };
-
-  uart_param_config(UART_NUM_0, &uart_config);
-  uart_param_config(UART_NUM_1, &uart_config);
-  uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-  uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
-
-  // Redirect ESP log to UART1
-  if (REDIRECT_LOGS) {
-    esp_log_set_vprintf(uart1_printf);
-  }
-}
-
-// Read UART_num for input with timeout of 1 sec
-int serial_read(char *buffer, int size) {
-  int len =
-      uart_read_bytes(UART_NUM, (uint8_t *)buffer, size, pdMS_TO_TICKS(1000));
-  return len;
-}
+#include "uart.c"
 
 void start_reading(void) {
+    char read_buffer[4];
     const char *uart_send_buffer;
-    uint16_t acc_array[3] = malloc(3 * sizeof(uint16_t));
     while (1) {
+        int read_len = serial_read(read_buffer, 4);
+        if (read_len > 0) {
+            if strcmp(read_buffer, "STOP") == 0 {
+                uart_write_bytes(UART_NUM, "OK\n", 3);
+                break;
+            }
+        }
         get_acc(acc_array);
         uart_send_buffer = (const char *)acc_array;
-        uart_write_bytes(UART_NUM, uart_send_buffer, 6);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        uart_write_bytes(UART_NUM, uart_send_buffer, 3 * sizeof(int16_t));
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void start_conn(void) {
+    char read_buffer[4];
+    const char *uart_send_buffer;
+    while (1) {
+        int read_len = serial_read(read_buffer, 4);
+        if (read_len > 0) {
+            if strcmp(read_buffer, "TEST") == 0 {
+                uart_write_bytes(UART_NUM, "OK\n", 3);
+            }
+            else if strcmp(read_buffer, "START") == 0 {
+                uart_write_bytes(UART_NUM, "OK\n", 3);
+                // Start reading
+                start_reading();
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
 void app_main(void) {
+    // Init UART
+    uart_setup();
+
+    // Init BME270
     ESP_ERROR_CHECK(bmi_init());
     softreset();
     chipid();
@@ -65,6 +59,7 @@ void app_main(void) {
     check_initialization();
     bmipowermode();
     internal_status();
-    printf("Comienza lectura\n\n");
-    
+
+    // Start reading
+    start_conn();
 }
